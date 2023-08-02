9ff97d92552ced8ad4a835dfab895086e63c78d5
dheerapat <dheerapat.t@kkumail.com>
2023-08-02 10:44:31 +0700

    init

---
+++ src/App.svelte
@@ -0,0 +1,16 @@
+<script>
+  import Main from "./Components/Main.svelte";
+  import Login from "./Components/Login.svelte";
+  import SignUp from "./Components/SignUp.svelte";
+  import PocketBase from 'pocketbase';
+  
+  const pb = new PocketBase('http://127.0.0.1:8090');
+</script>
+
+<main>
+  <Login />
+  <SignUp />
+</main>
+
+<style>
+</style>
---
+++ src/Components/Main.svelte
@@ -0,0 +1,9 @@
+<script>
+    import PocketBase from 'pocketbase';
+    const pb = new PocketBase('http://127.0.0.1:8090');
+</script>
+
+<div>
+    <h1>Hello</h1>
+    <p>please click this link</p>
+</div>
